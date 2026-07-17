e785d1642a12236c564c776f5bf41b2948e882d9
fattakhova003-wq <fattakhova003@gmail.com>
2026-07-17 13:26:26 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,7 +5,7 @@ keyboard_start = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                "🚀 НАЧАТЬ ОПЕРАЦИЮ",
+                "📄 ПОЛУЧИТЬ ПЕРВЫЙ ДОКУМЕНТ",
                 callback_data="document"
             )
         ]
@@ -31,78 +31,6 @@ keyboard_wait = InlineKeyboardMarkup(
             InlineKeyboardButton(
                 "🛰 ОЖИДАТЬ ДАЛЬНЕЙШИХ ИНСТРУКЦИЙ",
                 callback_data="wait"
-            )
-        ]
-    ]
-)
-
-
-keyboard_road = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                "➡️ ПРОДОЛЖИТЬ ОПЕРАЦИЮ",
-                callback_data="road"
-            )
-        ]
-    ]
-)
-
-
-keyboard_hotel = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                "🏨 ПОДТВЕРДИТЬ ОТЕЛЬ",
-                callback_data="hotel"
-            )
-        ]
-    ]
-)
-
-
-keyboard_aerotube = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                "🌪 ЗАПУСТИТЬ АКТИВНОСТЬ",
-                callback_data="aerotube"
-            )
-        ]
-    ]
-)
-
-
-keyboard_football = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                "⚽ ПЕРЕЙТИ К ИСПЫТАНИЮ",
-                callback_data="football"
-            )
-        ]
-    ]
-)
-
-
-keyboard_lounge = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                "🛋 ОТКРЫТЬ ДОСТУП",
-                callback_data="lounge"
-            )
-        ]
-    ]
-)
-
-
-keyboard_final = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                "🔐 ЗАВЕРШИТЬ ОПЕРАЦИЮ",
-                callback_data="final"
             )
         ]
     ]
